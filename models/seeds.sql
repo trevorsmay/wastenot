c9013d27208168fa6beed25f5fa1a6e52be19696
Trevor May <trevormay@Trevors-MacBook-Pro.local>
2019-07-06 13:20:10 -0600

    movement seeds

---
+++ models/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO jobs (foodType, donateQuantity, foodExpire, pickupLocation, pickupTime, price, createdAt, updatedAt)
+VALUES ("Carrots", "12", "07/24/2019", "123 Boulder Way, Boulder CO, 80344", "20:00", "$10.00", "2019-07-06", "2019-07-06");
+
+INSERT INTO jobs (foodType, donateQuantity, foodExpire, pickupLocation, pickupTime, price, createdAt, updatedAt)
+VALUES ("Apples", "20", "08/01/2019" "123 Golden Way, Golden CO, 80234", "14:00", "$25.00", "2019-07-06", "2019-07-06");
+
+INSERT INTO jobs (foodType, donateQuantity, foodExpire, pickupLocation, pickupTime, price, createdAt, updatedAt)
+VALUES ("Bananas", "5", "07/14/2019" "123 Denver Way, Denver CO, 80204", "15:00", "$15.00", "2019-07-06", "2019-07-06");
+
+INSERT INTO jobs (foodType, donateQuantity, foodExpire, pickupLocation, pickupTime, price, createdAt, updatedAt)
+VALUES ("Mixed Vegetables","30", "08/12/2019", "123 Fort Collins Way, Fort Collins CO 80001", "10:00", "$35.00", "2019-07-06", "2019-07-06");
+
+
+INSERT INTO Users (firstName, lastName, companyName, phoneNumber, email, password, location, photo, timesDonated, timesVolunteered, moneyEarned, itemsSold)
+VALUES ("Donald", "Duck", "Disney", "7205411345", "therealdonald@gmail.com", "Trumpsucks100", "Los Angeles, CA", "200", "0", "300,000");
+
+INSERT INTO Users (firstName, lastName, companyName, phoneNumber, email, password, location, photo, timesDonated, timesVolunteered, moneyEarned, itemsSold)
+VALUES ("Frank", "Reynolds", "Paddy's Pub", "8084445555", "frankthetank@gmail.com", "Franklovesbats", "Philadelphia, PA", "0", "20");
+
+INSERT INTO Users (firstName, lastName, companyName, phoneNumber, email, password, location, photo, timesDonated, timesVolunteered, moneyEarned, itemsSold)
+VALUES ("Fredo", "Corleone", "Corleone Olive Oil", "8003921234", "backstab@gmail.com", "Sleepinwithfish", "Reno, NV", "10", "10,000");
+
+INSERT INTO Users (firstName, lastName, companyName, phoneNumber, email, password, location, photo, timesDonated, timesVolunteered, moneyEarned, itemsSold)
+VALUES ("Michael", "Corleone", "Corleone Olive Oil", "8003921234", "thedon@gmail.com", "Trustnoone", "Reno, NV", "15", "400,000");
